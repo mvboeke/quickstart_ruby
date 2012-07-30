2ea852b3e45e376891ef969635cb525daff08a7b
Braintree <code@getbraintree.com>
2012-07-30 14:15:51 -0500

    Braintree Quickstart with Ruby

---
+++ app.rb
@@ -0,0 +1,32 @@
+require "sinatra"
+require "braintree"
+require "shotgun"
+
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = "QuickStart_Merchant"
+Braintree::Configuration.public_key = "QuickStart_Public"
+Braintree::Configuration.private_key = "QuickStart_Private"
+
+get '/' do
+  tr_data = Braintree::TransparentRedirect.transaction_data(
+    :redirect_url => "http://localhost:9393/braintree",
+    :transaction => {
+      :type => "sale",
+      :amount => "1000.00"
+    }
+  )
+
+  erb :form, :locals => {:tr_data => tr_data}
+end
+
+get "/braintree" do
+  result = Braintree::TransparentRedirect.confirm(request.query_string)
+
+  if result.success?
+     message = "Transaction Successful"
+  else
+     message = result.errors
+  end
+
+  erb :response, :locals => {:message => message}
+end
